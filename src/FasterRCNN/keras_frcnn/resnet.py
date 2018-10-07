fc536f48c73052995874058942b896bbd4ab6910
Felipe <bones.felipe@gmail.com>
2018-10-07 13:53:58 -0700

    fixing pretrained models paths

--- src/FasterRCNN/keras_frcnn/resnet.py
+++ src/FasterRCNN/keras_frcnn/resnet.py
@@ -18,9 +18,9 @@ from keras_frcnn.FixedBatchNormalization import FixedBatchNormalization
 
 def get_weight_path():
     if K.image_dim_ordering() == 'th':
-        return 'resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
+        return 'pretrained-models/resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
     else:
-        return 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+        return 'pretrained-models/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
 
 def get_img_output_length(width, height):
     def get_output_length(input_length):
